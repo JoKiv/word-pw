23a0c9f76017b3dafd0df70b07ec2f0c91b1acc1
Jouko <JoKiv@users.noreply.github.com>
2024-04-06 16:04:57 +0300

    word_count and word_separator variables

--- pw.py
+++ pw.py
@@ -6,13 +6,15 @@ import pickle
 import gzip
 
 words = ''
+word_count = 8
+word_separator =' '
 
 # Load the words array using pickle (using gzip compression)
 with gzip.open('words.pkl', 'rb') as f:
     words = pickle.load(f)
 
 # Generate a random sample of 8 words from the loaded words array
-random_words = random.sample(words, 8)
+random_words = random.sample(words, word_count)
 
 # Print the randomly selected words
-print(" ".join(random_words))
+print(word_separator.join(random_words))
